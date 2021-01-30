94b536b06da742347bdb2c4ec09c4132acc2dcc6
pHr0723 <pHr0723@Pao.local>
2021-01-29 22:05:02 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+
+  const url = 'http://api.weatherstack.com/current?access_key=d39d00e753663976ca1581ba1e2f57c1&query=' + latitude + ',' + longitude + '&units=f'
+
+  request ({ url, json: true}, (error, {body}) => {
+  
+    if(error) {
+
+        callback('Unable to find services!', undefined)
+    }else if(body.error){
+          callback('Unable to find location', undefined)
+    }else{
+        callback(undefined,body.current.temperature)
+      }
+  })
+}
+
+module.exports = {
+  forecast : forecast
+}
